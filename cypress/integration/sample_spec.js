53a5096dc30b4c6e3c1346188f0aeeeb53235864
Nic Johnson <iheartredsox@gmail.com>
2020-01-07 22:22:34 -0700

    Murder CORS

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,31 @@
 describe('FF payment form', function() {
   it('Opens the payment form', function() {
     cy.visit('https://dev.fundfairy.org/donate/?donation=5&email=iheartredsox@gmail.com')
-    cy.get('#black-credit-card-button').click()
+    // cy.get('iframe.component-frame').iframeLoaded().its('document').getInDocument('#black-credit-card-button').trigger('click')
+    // cy.get('iframe.component-frame').within(($iframe) => {
+    //   cy.get('iframe').iframeLoaded().its('document').getInDocument('#credit-card-number').type('4443257862494732')
+    // })
+    cy.get('iframe.component-frame')
+    .then(function ($iframe) {
+      const $body = $iframe.contents().find('body')
+      
+      cy
+        .wrap($body)
+        .find('#black-credit-card-button')
+        .trigger('click')
+
+      cy.get('iframe')
+      .then(function ($iframe2) {
+        const $body2 = $iframe2.contents().find('body')
+
+        cy
+          .wait(5000)
+        
+        cy
+          .wrap($body2)
+          .find('#credit-card-number')
+          .type('4443257862494732')
+      })
+    })  
   })
 });
